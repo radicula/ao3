e56bd7a3071839706a5b0ac950f0a5f6db9e4486
radicula <radishfan13@gmail.com>
2023-04-15 21:23:35 -0400

    Add search functionality

--- ao3-stats.py
+++ ao3-stats.py
@@ -8,12 +8,49 @@ Not linted. Sorry in advance.
 """
 
 import AO3
-import tqdm
+from math import ceil
+from tqdm import tqdm
+
+
+def tag_search(tag, page=1):
+    search = AO3.Search(tags=tag)
+    search.page = page
+    search.update()
+    return search
+
+
+def print_results(search):
+    # Just for debugging help
+    print(search.total_results)
+    for result in search.results:
+        print(result)
+
 
 def main():
-    tag = input("\nWhat tag would you like to investigate? ")
-    print("Investigating '{}'...\n".format(tag))
+    #tag = input("\nWhat tag would you like to investigate? ")
+    #print("Investigating '{}'...".format(tag))
+    tag = "Lucy Carlyle/George Cubbins | George Karim/Anthony Lockwood"
+    tag = "Protective Quill Kipps"
+
+    search = tag_search(tag)
+    num_pages = ceil(float(search.total_results)/20.0)
+
+    print("Processing {} pages of search results ...".format(num_pages))
+    work_ids = []
+    for page in tqdm(range(1, num_pages + 1)):
+        s = tag_search(tag, page)
+        work_ids = work_ids + [r.id for r in search.results]
+
+    print("Processing {} fics (this may take some time) ...".format(len(work_ids)))
+    works = []
+    for work_id in tqdm(work_ids):
+        works.append(AO3.Work(work_id))
+
+    print("")
+
+    #print(work.date_published)
     return
+
 
 if __name__ == "__main__":
     main()
