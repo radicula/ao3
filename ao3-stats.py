8e4c9fdf6f657f87c694129eaea075d7d65e455a
radicula <radishfan13@gmail.com>
2023-04-15 21:58:58 -0400

    Complete basic functionality

--- ao3-stats.py
+++ ao3-stats.py
@@ -8,6 +8,9 @@ Not linted. Sorry in advance.
 """
 
 import AO3
+import json
+import time
+from datetime import datetime
 from math import ceil
 from tqdm import tqdm
 
@@ -25,12 +28,23 @@ def print_results(search):
     for result in search.results:
         print(result)
 
+def get_work_id(search_result, num_retries=5, wait_time=5):
+    while num_retries > 0:
+        try:
+            work_id = search_result.id
+            return work_id
+        except HTTPError:
+            print("Rate limit exceeded. Waiting {} seconds " +
+                    "({} retries remaining)...".format(wait_time, num_retries))
+            time.sleep(wait_time)
+            num_retries -= 1
+    print("Something is very wrong. Skipping this work; entire process may fail.")
+    return []
 
 def main():
-    #tag = input("\nWhat tag would you like to investigate? ")
-    #print("Investigating '{}'...".format(tag))
-    tag = "Lucy Carlyle/George Cubbins | George Karim/Anthony Lockwood"
-    tag = "Protective Quill Kipps"
+    tag = input("\nWhat tag would you like to investigate? " + 
+            "(copy and paste it exactly!) ")
+    print("Investigating '{}'...".format(tag))
 
     search = tag_search(tag)
     num_pages = ceil(float(search.total_results)/20.0)
@@ -39,16 +53,52 @@ def main():
     work_ids = []
     for page in tqdm(range(1, num_pages + 1)):
         s = tag_search(tag, page)
-        work_ids = work_ids + [r.id for r in search.results]
+        for result in s.results:
+            work_ids.append(get_work_id(result))
 
     print("Processing {} fics (this may take some time) ...".format(len(work_ids)))
     works = []
     for work_id in tqdm(work_ids):
         works.append(AO3.Work(work_id))
 
+    work_info = {}
+    for work in works:
+        work_info['id'] = work.id
+        work_info['bookmarks'] = work.bookmarks
+        work_info['categories'] = work.categories
+        work_info['nchapters'] = work.nchapters
+        work_info['characters'] = work.characters
+        work_info['complete'] = work.complete
+        work_info['comments'] = work.comments
+        work_info['expected_chapters'] = work.expected_chapters
+        work_info['fandoms'] = work.fandoms
+        work_info['hits'] = work.hits
+        work_info['kudos'] = work.kudos
+        work_info['language'] = work.language
+        work_info['rating'] = work.rating
+        work_info['relationships'] = work.relationships
+        work_info['restricted'] = work.restricted
+        work_info['status'] = work.status
+        work_info['summary'] = work.summary
+        work_info['tags'] = work.summary
+        work_info['title'] = work.title
+        work_info['warnings'] = work.warnings
+        work_info['words'] = work.words
+        work_info['collections'] = work.collections
+        work_info['date_edited'] = str(work.date_edited)
+        work_info['date_published'] = str(work.date_published)
+        work_info['date_updated'] = str(work.date_updated)
+
+    time = datetime.now()
+    filename = "ao3_stats_{}_{}.json".format(
+            ''.join(l for l in tag if l.isalnum()),
+            str(time.year) + str(time.month) + str(time.day))
+    print("Saving results to '{}'...".format(filename))
+    with open(filename, "w") as filepath:
+        json.dump(work_info, filepath, indent = 4)
+    print("Done!")
     print("")
 
-    #print(work.date_published)
     return
 
 
